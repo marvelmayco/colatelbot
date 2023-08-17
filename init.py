7ff7cd3137324a336c81d1df3b7f799588c82bb0
marvelmayco <108984569+marvelmayco@users.noreply.github.com>
2023-08-17 23:15:07 +0530

    Update init.py

--- init.py
+++ init.py
@@ -3,18 +3,13 @@
 # Creamos un archivo .env
 archivo = open(".env", "w")
 
-# Pedimos al usuario que ingrese un string
-string1 = input("Envieme su API de OpenAi: ")
-
 # Pedimos al usuario que ingrese otro string
-string2 = input("Envieme el token de Telegram: ")
+string2 = input("Enter BOT TOKEN ID: ")
 
-# Añadimos los strings al archivo
-archivo.write("API_KEY="+string1 + "\n")
 archivo.write("TOKEN="+string2 + "\n")
 
 # Cerramos el archivo
 archivo.close()
 
 # Mostramos un mensaje de confirmación
-print("Listo, ya puede continuar con el proximo paso")
+print("Data Saved!!!")
