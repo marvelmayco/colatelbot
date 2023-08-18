c5e378d215fa875393bb54944ab54417ff82f259
marvelmayco <108984569+marvelmayco@users.noreply.github.com>
2023-08-19 03:10:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def bytes_to_human_readable(size_bytes):
 
 def get_progress_bar(percentage):
     bars = int(percentage / 10)
-    return "🔵" * bars + "⚪" * (10 - bars)
+    return "⬛" * bars + "⬜" * (10 - bars)
 
 def format_eta(seconds):
     if seconds < 60:
